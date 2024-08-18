b6036b864a1e0225cce1608886127360a10f710f
nanauda <141910752+nanauda@users.noreply.github.com>
2024-08-19 01:33:16 +0900

    Create answer.ts

---
+++ questions/00014-easy-first/answer.ts
@@ -0,0 +1,43 @@
+/*
+  14 - First of Array
+  -------
+  by Anthony Fu (@antfu) #easy #array
+
+  ### Question
+
+  Implement a generic `First<T>` that takes an Array `T` and returns its first element's type.
+
+  For example:
+
+  ```ts
+  type arr1 = ['a', 'b', 'c']
+  type arr2 = [3, 2, 1]
+
+  type head1 = First<arr1> // expected to be 'a'
+  type head2 = First<arr2> // expected to be 3
+  ```
+*/
+
+/* _____________ Your Code Here _____________ */
+// インデックスアクセス型 (indexed access types)を使う。これだとFirst<[]>のときにneverでなくundefinedが返ってしまい満たさない
+type FirstNg<T extends any[]> = T[0] 
+
+// Conditional Typesでnever型を返す
+type First<T extends any[]> = T extends [] ? never : T[0] 
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<First<[3, 2, 1]>, 3>>,
+  Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
+  Expect<Equal<First<[]>, never>>,
+  Expect<Equal<First<[undefined]>, undefined>>,
+]
+
+type errors = [
+  // @ts-expect-error
+  First<'notArray'>,
+  // @ts-expect-error
+  First<{ 0: 'arrayLike' }>,
+]
